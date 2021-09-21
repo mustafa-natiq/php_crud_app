c24fdd36b41a3b985358a3f681cb72631c0f4c8b
olorondu-emeka <oloronduchukwuemeka@gmail.com>
2021-09-21 18:48:57 +0100

    added server start function in pipeline

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -1,18 +1,35 @@
-<?php declare(strict_types=1);
+<?php
 
-namespace Tests;
+require_once 'vendor/autoload.php';
 
 use PHPUnit\Framework\TestCase;
+use GuzzleHttp\Client;
 
 final class UserTest extends TestCase{
+    private $http;
+
+    protected function setUp(): void
+    {
+        $this->http = new Client([
+            'base_uri' => 'http://localhost:5000'
+        ]);
+    }
+
+    public function tearDown(): void {
+        $this->http = null;
+    }
+
     /**
      * @test
      */
     public function should_create_new_user(){
         $user = $this->userData();
         //post data to registration end-point
+        
         // $this->post('/api/v1/register', $user);
-        $this->assertEquals(2, 2);
+        $response = $this->http->post('/users', $user);
+
+        $this->assertEquals(201, $response->getStatusCode());
     }
 
     private function userData(){
